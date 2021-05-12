2a84864ac25d7f7c08099146026a7f33e1e84026
srujan338 <79457460+srujan338@users.noreply.github.com>
2021-05-12 18:50:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# car-racing-1
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+var canvas,backgroundImg;
+var gameState = 0; 
+var playerCount;
+var form , game , player;
+
+function setup(){
+    database = firebase.database();
+    canvas = createCanvas(400,400);
+
+    game= new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    
+}
